272188bd00a40924550b37f7d5c0ae478a4eac83
lazycat7777 <timur5554@mail.ru>
2024-11-12 16:09:19 +0300

    html страница. Подправил таблицу

--- screener_daily_USA/views.py
+++ screener_daily_USA/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Stock_Data_Indicators
+from .models import Stock_Merge_All_Tables
 import json
 
 def index_html(request):
     return render(request, 'index.html')
 
 def screener_html(request):
-    stock_data = Stock_Data_Indicators.objects.all().values()
+    stock_data = Stock_Merge_All_Tables.objects.all().values()
     data = list(stock_data) 
     return render(request, 'screener.html', {'data': json.dumps(data)}) 
 
