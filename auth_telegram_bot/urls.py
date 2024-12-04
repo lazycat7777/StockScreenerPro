40ca5660ea103ce4a7ab0f3ca7d5bcb3ca5b8819
lazycat7777 <timur5554@mail.ru>
2024-12-05 02:16:58 +0300

    обновил ссылки в html страницах, подключил все друг к другу

--- auth_telegram_bot/urls.py
+++ auth_telegram_bot/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'auth_telegram_bot'
+
 urlpatterns = [
     path('to_telegram_bot/', views.to_telegram_bot_html, name='to_telegram_bot'),
     path('telegram_bot_access/', views.telegram_bot_access_html, name='telegram_bot_access'),
--- screener_daily_USA/urls.py
+++ screener_daily_USA/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'screener_daily_USA'
+
 urlpatterns = [
     path('screener/', views.screener_html, name='screener'),
     path('stock_heatmap_SPX/', views.stock_heatmap_SPX_html, name='stock_heatmap_SPX'),
