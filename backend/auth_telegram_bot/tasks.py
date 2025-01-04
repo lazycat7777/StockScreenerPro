d8cad3cd54f84f6abae2fc8024de6efb84101744
lazycat7777 <timur5554@mail.ru>
2025-01-04 18:44:20 +0300

    добавил код certbot для автоматизации получения и обновления сертификатов; добавил nginx отдельно для разработки и отдельно для продакшена; обновлены селери таски, настройки celery и celery beat; обновлены html; изменены пути статических файлов; изменены ссылки в тг боте и сайте для продакшена; обновлен docker compose; добавлена яндекс метрика

--- backend/auth_telegram_bot/tasks.py
+++ backend/auth_telegram_bot/tasks.py
@@ -1,7 +1,16 @@
-from celery import shared_task
+import time
+from datetime import timedelta
+from screener_website.celery_app import app
 from .service import cleanup_expired_access
 
 # Удаляем устаревшие записи IP адресов
-@shared_task
+@app.task
 def remove_expired_ip_access():
+    start_time = time.time()  
+
     cleanup_expired_access()
+
+    end_time = time.time()  
+    elapsed_time = end_time - start_time  
+    elapsed_time_formatted = str(timedelta(seconds=elapsed_time))
+    print(f'Селери таска завершена, устаревшие записи IP адресов удалены. Время выполнения: {elapsed_time_formatted}')
--- backend/screener_website/celery_app.py
+++ backend/screener_website/celery_app.py
@@ -6,17 +6,17 @@ from celery.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'screener_website.settings')
 
 app = Celery('screener_website')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'calculate_results_USA': {
         'task': 'screener_daily_USA.tasks.calculate_results_USA',
-        'schedule': crontab(minute=0, hour=3),
+        'schedule': crontab(hour=3, minute=0),
     },
     'cleanup_expired_ip_access': {
         'task': 'auth_telegram_bot.tasks.remove_expired_ip_access',
-        'schedule': crontab(minute=0, hour=0),
+        'schedule': crontab(hour=0, minute=0),
     }
 }
